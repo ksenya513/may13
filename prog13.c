4e75bfc57187ae015e36f05ea28cce5df20bf95a
ksenya513 <karpickaamasya@gmail.com>
2025-10-07 00:52:23 +0700

    Create prog13.c

---
+++ prog13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char** argv){
+  int array[5];
+  printf("Введите пять целых чисел:\n";
+  for (int i=0; i<5; i++){
+    scanf("%d",&array[i]);
+  }
+  return 0;
+}
